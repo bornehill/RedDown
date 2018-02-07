e80d5a5fc98754d36e06d9c1a6ac5419b7f173d7
bornehill <ali.escamilla.navarro@gmail.com>
2018-02-07 00:28:30 -0600

    First version

---
+++ RedDown/ViewModels/ParentViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedDown.ViewModels
+{
+    public class ParentViewModel
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "El nombre es requerido (min. 3 caracteres)")]
+        public string Name { get; set; }
+        [Required]
+        [MinLength(5, ErrorMessage = "El A. paterno es requerido (min. 5 caracteres)")]
+        public string App { get; set; }
+        [MinLength(5, ErrorMessage = "A. materno (min. 5 caracteres)")]
+        public string Apm { get; set; }
+        public int AgeBirth { get; set; }
+        public int Pupilage { get; set; }
+        public int RelationShip { get; set; }
+    }
+}
---
+++ RedDown/ViewModels/RegistryDownViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedDown.ViewModels
+{
+    public class RegistryDownViewModel
+    {
+        [Required]
+        [MinLength(3, ErrorMessage ="El nombre es requerido (min. 3 caracteres)")]
+        public string Name { get; set; }
+        [Required]
+        [MinLength(5, ErrorMessage = "El A. paterno es requerido (min. 5 caracteres)")]
+        public string App { get; set; }
+        
+        [MinLength(5, ErrorMessage = "A. materno (min. 5 caracteres)")]
+        public string Apm { get; set; }
+        [Required]
+        public DateTime BirthDate { get; set; }
+        [Required]
+        public int BirthPlace { get; set; }
+        [Required]
+        public int ResidencePlace { get; set; }
+        [Required]
+        public ParentViewModel Father { get; set; }
+        [Required]
+        public ParentViewModel Mother { get; set; }
+    }
+}
